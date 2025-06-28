50e0957455aa8db2e11858a9c98f40cc72a545a4
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-06-28 08:54:55 -0300

    chore: finish the basic template of react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>Home</h1>,
+  },
+  {
+    path: "/signup",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 

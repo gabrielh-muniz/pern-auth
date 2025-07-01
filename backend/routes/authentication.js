964eb1f2a05051e6700cbe09c55ffc39d128625f
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-01 15:03:04 -0300

    feat: add OAuth routes

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -7,8 +7,10 @@ import {
   forgotPassword,
   resetPassword,
   checkAuth,
+  oauthCallback,
 } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import passport from "../config/passport.js";
 
 export const router = Router();
 
@@ -20,3 +22,14 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
 router.get("/check-auth", verifyToken, checkAuth);
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  oauthCallback
+);

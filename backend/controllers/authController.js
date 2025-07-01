bc8c6afddd6172bc735add193abb5d888885eafd
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-01 16:13:56 -0300

    fix: change jwtToken to token in order to match the oauthCallback function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -360,12 +360,12 @@ export async function checkAuth(req, res) {
  * @returns {Promise<void>}
  */
 export async function oauthCallback(req, res) {
-  const { jwtToken } = req.user || {};
-  if (!jwtToken) {
+  const { token } = req.user || {};
+  if (!token) {
     console.error("No JWT token found");
     return res.redirect(`${process.env.FRONTEND_URL}/`);
   }
-  res.cookie("token", jwtToken, {
+  res.cookie("token", token, {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });

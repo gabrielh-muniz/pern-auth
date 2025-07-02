668fce48b1534445b97364963c7be7ef974d9c0d
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-02 20:12:34 -0300

    feat: add the increment click count router and its controller function

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -17,3 +17,28 @@ export async function fetchGameData(req, res) {
 
   return res.status(200).json({ gameData });
 }
+
+/**
+ * Function to increment the click count for the authenticated user.
+ * @param {Object} req - The request object containing user information.
+ * @param {Object} res - The response object to send the updated click count.
+ * @returns {Object} - Returns the updated click count for the authenticated user.
+ */
+export async function incrementClickCount(req, res) {
+  const userId = req.user.id;
+  const { clickLevel } = req.body;
+
+  const [error, { rows }] = await catchError(
+    query(
+      `
+      INSERT INTO click_stats (user_id, clicks, click_level, last_click_at) VALUES ($1, 1, $2, NOW())
+      ON CONFLICT (user_id) 
+      DO UPDATE SET clicks = click_stats.clicks + 1, click_level = $2, last_click_at = EXCLUDED.last_click_at RETURNING *
+      `,
+      [userId, clickLevel]
+    )
+  );
+  if (error) return res.status(500).json({ message: "Database query failed" });
+
+  return res.status(200).json(rows[0]);
+}
--- backend/routes/game.js
+++ backend/routes/game.js
@@ -3,4 +3,6 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 
 export const router = Router();
 
+router.post("/increment", verifyToken, incrementClickCount);
+
 router.get("/me", verifyToken, fetchGameData);

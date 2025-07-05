6fdbf4ac41d16ec4f36409d1ebad9c5b89f63779
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-07-05 11:17:49 -0300

    chore: send the refresh token as http-only cookie in the google callback function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -390,7 +390,7 @@ export async function checkAuth(req, res) {
  * @returns {Promise<void>}
  */
 export async function oauthCallback(req, res) {
-  const { token } = req.user || {};
+  const { token, refreshToken } = req.user || {};
   if (!token) {
     console.error("No JWT token found");
     return res.redirect(`${process.env.FRONTEND_URL}/`);
@@ -398,6 +398,11 @@ export async function oauthCallback(req, res) {
   res.cookie("token", token, {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
   res.redirect(`${process.env.FRONTEND_URL}/dashboard`);
 }
